9bc1c5907e948e5c2f9e01d9c9546e9ca55c7bb9
Magnus Erixzon <magnus@erixzon.com>
2010-07-08 18:34:38 +0800

    fix xml key

--- lib/WWW/Google/Contacts/Type/Jot.pm
+++ lib/WWW/Google/Contacts/Type/Jot.pm
@@ -21,7 +21,7 @@ has value => (
     isa       => Str,
     is        => 'rw',
     traits    => [ 'XmlField' ],
-    xml_key   => 'value',
+    xml_key   => 'content',
     predicate => 'has_value',
     required  => 1,
 );
