0f0c96fe8488c58517833ff58c40d24927465e35
Magnus Erixzon <magnus@erixzon.com>
2010-07-08 23:38:08 +0800

    New version, as previous dist had some junk in it

--- lib/WWW/Google/Contacts.pm
+++ lib/WWW/Google/Contacts.pm
@@ -12,7 +12,7 @@ use WWW::Google::Contacts::ContactList;
 use WWW::Google::Contacts::Group;
 use WWW::Google::Contacts::GroupList;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 $VERSION = eval $VERSION;
 
 has username => (
